51b7356ce9c6d295e3faed02fcd0dc18fc6d2d20
CAROLINE <carolinesala16@gmail.com>
2019-03-23 21:58:40 -0300

    Adição da geração de solução para inteiro permutados

--- ag.cpp
+++ ag.cpp
@@ -7,10 +7,15 @@ Tamanho do cromossomo (D)
 Se codificação binária, representa a quantidade de bits do cromossomo
 Se codificação real, representa a quantidade de variáveis sendo otimizada (número de dimensões do problema)
 Domínio das variáveis: Bounds = [Li, Ui] Є R
-Se codificação inteira, representa a quantidade de inteiros a se         considerar (VERIFICAR SE É PERMUTAÇÃO)
+Se codificação inteira, representa a quantidade de inteiros a se         
+considerar (VERIFICAR SE É PERMUTAÇÃO)
 Domínio das variáveis: Bounds = [Li, Ui] Є Z
 Se permutação (INT-PERM), D representa a quantidade de inteiros a permutar
 
+*/
+
+/*Compilar com flag 
+$ g++ --std=c++11 ag.cpp -o teste
 */
 
 
@@ -20,7 +25,7 @@ using namespace std;
 
 int main(){
   char COD;
-  int POP, D;
+  int POP, D, aux;
   double L, U;
   //default_random_engine generator;
   cin >> COD >> POP >> D >> L >> U;
@@ -28,16 +33,26 @@ int main(){
   uniform_real_distribution<double> distribuicaoReal(L, U);
   uniform_int_distribution<int> distribuicaoInteira((int)L, (int)U);
   mt19937_64 gerador(semente);
+  //Vetor do inteiro permutado
+  int permuta[D] = {0};
+  //Zerei vetor
+  for(int i = 0; i < D; i++){
+    permuta[i] = 0;
+  }
+
+
 
   //Espaço de busca em double para ser utilizado também por inteiros e binários
   vector<vector <double>> populacao (POP, vector<double>());
 
   //Verifica qual é a codificação
   switch (COD) {
-    //Se B = Binária
+    /*Se B = Binária
+      L=0 e U=1
+    */
     case 'B':
     if(L!=0 || U!=1){
-      cout << "Insira um limite binário: 0 1" << endl;
+      cout << "Erro: Insira um limite binário: 0 1" << endl;
       break;
     }else {
       for (int i=0; i < POP; i++) {
@@ -69,8 +84,37 @@ int main(){
         cout << endl;
       }
       break;
-    //Se P = Inteiro Permutado
+    /*Se P = Inteiro Permutado
+      L=0 e U=D-1
+    */
     case 'P':
+      //Verifica integridade dos parametros
+      if(!(L==0 && U==D-1)){
+        cout << "Erro: Corrija os parametros L e U para L=0 e U=D-1" << endl;
+        break;
+      }
+      for (int i=0; i < POP; i++) {
+        for(int j=0; j< D; j++){
+          aux = distribuicaoInteira(gerador);
+          
+          while(permuta[aux] == 1){
+            aux = distribuicaoInteira(gerador);
+          }
+          populacao[i].push_back(aux);
+          permuta[aux] = 1;
+        }
+          for(int k = 0; k < D; k++){
+            permuta[k] = 0;
+          }
+      }
+      
+      for (int i=0; i < POP; i++) {
+        for(int j=0; j< D; j++){
+        //  printf("%02d   ", (int)populacao[i][j] );
+          cout << populacao[i][j] << "    ";
+        }
+        cout << endl;
+      }
       break;
     //Se R == Real
     case 'R':
